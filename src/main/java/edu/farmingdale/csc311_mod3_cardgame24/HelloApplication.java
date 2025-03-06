14fcc51403f62ae3e278a6847c3c38b81d31891b
Kathleen Wims <bewimsical@gmail.com>
2025-03-05 20:15:51 -0500

    added hint popup and rule popup

--- src/main/java/edu/farmingdale/csc311_mod3_cardgame24/HelloApplication.java
+++ src/main/java/edu/farmingdale/csc311_mod3_cardgame24/HelloApplication.java
@@ -11,8 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 400
-        );
+        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
         stage.setTitle("24");
         stage.setScene(scene);
         stage.show();
--- src/main/java/edu/farmingdale/csc311_mod3_cardgame24/HelloController.java
+++ src/main/java/edu/farmingdale/csc311_mod3_cardgame24/HelloController.java
@@ -2,62 +2,60 @@ package edu.farmingdale.csc311_mod3_cardgame24;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import javax.script.ScriptException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
-    @FXML
-    private ImageView cardView1;
+    @FXML private ImageView cardView1;
 
-    @FXML
-    private ImageView cardView2;
+    @FXML private ImageView cardView2;
 
-    @FXML
-    private ImageView cardView3;
+    @FXML private ImageView cardView3;
 
-    @FXML
-    private ImageView cardView4;
+    @FXML private ImageView cardView4;
 
-    @FXML
-    private TextField expression;
+    @FXML private TextField expression;
 
-    @FXML
-    private Label expressionLabel;
+    @FXML private Label expressionLabel;
 
-    @FXML
-    private TextField solution;
+    @FXML private TextField solution;
 
     private ImageView[] cardViews;
 
     private Deck deck;
     private int[] cards;
-    private int cardValue1;
-    private int cardValue2;
-    private int cardValue3;
-    private int cardValue4;
+
     private String solverScript;
+
     private int hintCount = 0;
     private String numSolutions = "0";
+    private String randomSolution ="";
+    private int solutionCount = 0;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         deck = new Deck();
         cardViews = new ImageView[]{cardView1, cardView2, cardView3, cardView4};
-        dealCards();
         solverScript = Utils.getScript();
+        dealCards();
     }
     @FXML
     public void dealCards(){
-        cards = new int[]{cardValue1, cardValue2, cardValue3, cardValue4};
+        cards = new int[4];
         if(deck.getDeck().size() >= 4) {
             for (int i = 0; i < 4; i++) {
                 Card c = deck.deal();
@@ -65,11 +63,26 @@ public class HelloController implements Initializable {
                 cards[i] = c.getValue();
             }
         }else deck = new Deck();
+
         expression.clear();
         expression.setPromptText("");
         expression.getStyleClass().removeAll("error", "solved");
         hintCount = 0;
         numSolutions = "0";
+        try {
+            numSolutions = Utils.getSolutionsCount(cards, solverScript);
+            System.out.println(numSolutions);
+        } catch (ScriptException e) {
+            System.out.println("ERROR BAD SCRIPT");
+        }
+        if (Integer.parseInt(numSolutions) > 0) {
+            try {
+                randomSolution = Utils.getRandomSolution(cards, solverScript);
+                System.out.println(randomSolution);
+            } catch (ScriptException e) {
+                System.out.println("ERROR BAD SCRIPT");;
+            }
+        }
     }
 
     @FXML
@@ -133,22 +146,67 @@ public class HelloController implements Initializable {
     @FXML
     void findSolution() {
         //Get number of solutions
-        String randomSolution = "";
-        if (hintCount == 0 ){
+        expression.getStyleClass().removeAll("error", "solved");
+        if (solutionCount == 0 && Integer.parseInt(numSolutions) > 0){
+            expression.setText(randomSolution);
+            solutionCount++;
+            return;
+        }else if (Integer.parseInt(numSolutions) > 0){
             try {
-                numSolutions = Utils.getSolutionsCount(cards, solverScript);
-                hintCount++;
-                System.out.println(numSolutions);
-            } catch (ScriptException e) {
-                System.out.println("ERROR BAD SCRIPT");
-            }
-        } else if (hintCount == 1 && Integer.parseInt(numSolutions) > 0) {
-            try {
-                randomSolution = Utils.getRandomSolution(cards, solverScript);
+                randomSolution = Utils.getRandomSolution(cards, solverScript, solutionCount%Integer.parseInt(numSolutions));
                 System.out.println(randomSolution);
+                expression.setText(randomSolution);
+                solutionCount++;
             } catch (ScriptException e) {
                 System.out.println("ERROR BAD SCRIPT");;
             }
+            return;
         }
+        expression.setText("no solutions");
+
+    }
+    @FXML
+    void clearExpression(MouseEvent event) {
+        expression.getStyleClass().removeAll("error", "solved");
+        expression.clear();
+    }
+    @FXML
+    void getHint(MouseEvent event) throws IOException {
+        Stage primaryStage = (Stage) expression.getParent().getScene().getWindow();
+        String hint1 = "There are "+numSolutions+" solutions";
+        String hint2 = randomSolution.replaceAll("[\\d]", "_");
+        System.out.println(hint2);
+        //popup
+        Stage popUp = new Stage();
+        popUp.initModality(Modality.APPLICATION_MODAL);
+        popUp.initOwner(primaryStage);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("popup-view.fxml"));
+        Scene popUpScene = new Scene(fxmlLoader.load(), 300,150);
+        popUp.setScene(popUpScene);
+        popUp.setTitle("Hints");
+        popUp.show();
+        PopupController pc = fxmlLoader.getController();
+        System.out.println(hintCount);
+        if (hintCount == 0){
+            pc.setHint(hint1);
+            hintCount++;
+        }else{
+            pc.setHint(hint1, hint2);
+        }
+
+    }
+    @FXML
+    void getRules(MouseEvent event) throws IOException{
+        System.out.println("getting rules");
+        Stage primaryStage = (Stage) expression.getParent().getScene().getWindow();
+        //popup
+        Stage popUp = new Stage();
+        popUp.initModality(Modality.APPLICATION_MODAL);
+        popUp.initOwner(primaryStage);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("rules-view.fxml"));
+        Scene popUpScene = new Scene(fxmlLoader.load(), 450,350);
+        popUp.setScene(popUpScene);
+        popUp.setTitle("Rules");
+        popUp.show();
     }
 }
